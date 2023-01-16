1654bcc661471fe833966a38aa2a76a8ca0cfb02
mainuzzamanbabu <mainuzzamanb@gmail.com>
2023-01-16 17:37:51 +0600

    Course of Django4 from udemy first commit

---
+++ my_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+def example_view(request):
+    #my_app/templates/my_app/example.html
+    return render(request,'my_app/example.html')
+
+def variable_view(request):
+    my_var = {'first_name': 'RosaLind', 'last_name': 'franklin',
+             'some_list':[1,2,3], 'some_dict':{'inside_key':'inside_value'}    
+            }
+    return render(request,'my_app/variable.html', context=my_var)
